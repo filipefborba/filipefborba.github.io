21c40be554d95054ed8233e26104878417689830
Filipe Borba <filipefborba@gmail.com>
2016-12-02 16:09:20 -0200

    p2_codesign

---
+++ js/add_convidado.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('.ap_box').append(ApEscolhido)
+	if (ApEscolhido === 101) {
+		$('#moradores').append('<li class="list_item">Marcelo H.</li>')
+		$('#moradores').append('<li class="list_item">Megan Fox</li>');	
+	}
+	
+})
--- js/convidados.js
+++ js/convidados.js
@@ -6,14 +6,43 @@ $(document).ready(function() {
 	$('.combobox').change(function() {
 	// obtendo o valor do atributo value da tag option
 	ApEscolhido = $(".combobox option:selected").val();
-	$('ap_box').css("display","block");
+	$('.ap_box').css("display","block");
 	if (ApEscolhido === '101') {
 		$('.ap_box').append('<p>101</p>')
 		$('ul').append('<li class="list_item">Marcelo H.</li>');
 		$('ul').append('<li class="list_item">Megan Fox</li>');
 		$('ul').append('<li class="list_item">Marcelo Jr. Fox</li>');
-		console.log(ApEscolhido);
-	}
+
+		$('#name1').css('display','inline');
+		$('#status1').css('display','inline-block');
+		$('#name2').css('display','inline');
+		$('#status2').css('display','inline-block');
+
+		}
+
+	else if (ApEscolhido === '102') {
+		$('.ap_box').append('<p>102</p>')
+		$('ul').append('<li class="list_item">Fabio A.</li>');
+		$('ul').append('<li class="list_item">Sra. A.</li>');
+	
+		$('#name3').css('display','inline');
+		$('#status3').css('display','inline-block');
+		$('#name4').css('display','inline');
+		$('#status4').css('display','inline-block');
+
+		}
+
+	else if (ApEscolhido === '103') {
+		$('.ap_box').append('<p>103</p>')
+		$('ul').append('<li class="list_item">Gabriel Noal</li>');
+		
+		$('#name5').css('display','inline');
+		$('#status5').css('display','inline-block');
+		$('#name6').css('display','inline');
+		$('#status6').css('display','inline-block');
+
+		}
+
 	});
 
 
