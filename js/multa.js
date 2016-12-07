ad4514d946fab07b24e0c170d952124f32fbc862
Filipe Borba <filipefborba@gmail.com>
2016-12-06 23:01:26 -0200

    p2_codesign

---
+++ js/multa.js
@@ -0,0 +1,5 @@
+function renable() {
+  document.getElementById("multa_button").disabled = false;
+  document.getElementById("ocorrencia").innerHTML = "Ocorrência 3/3";
+  document.getElementById("detalhe").innerHTML = "O condômino infringiu as regras de boa-convivência do condomínio, fazendo barulho demais e perturbando os vizinhos após às 22h de hoje.";
+}
