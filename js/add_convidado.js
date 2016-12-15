6dba0bdd59c30f0a932c12d1c6751189f1e1a823
Filipe Borba <filipefborba@gmail.com>
2016-12-15 00:25:40 -0200

    Portfolio

--- js/add_convidado.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('.ap_box').append(ApEscolhido)
-	if (ApEscolhido === 101) {
-		$('#moradores').append('<li class="list_item">Marcelo H.</li>')
-		$('#moradores').append('<li class="list_item">Megan Fox</li>');	
-	}
-	
-})
